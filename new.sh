b7215440a9e679ca6c7ca99bcd29cc4431775dd9
George Bonner <georgebonnr@gmail.com>
2016-01-25 02:37:48 -0800

    set up remote branch

--- new.sh
+++ new.sh
@@ -10,8 +10,10 @@ MODIFIER=$2
 if [ "$MODIFIER" == "npm" ]; then
     CLONE_FROM="npm-es2015-starter"
 fi
+curl -u georgebonnr https://api.github.com/user/repos -d '{ "name": "'$NEW_REPO_NAME'" }' && \
 printf "\n Clone source: $CLONE_FROM:\n\n"
-git clone git@github.com:georgebonnr/$CLONE_FROM.git $NEW_REPO_NAME\
- && cd ./$NEW_REPO_NAME && git remote rm origin\
- && git remote add origin git@github.com:georgebonnr/$NEW_REPO_NAME.git\
- && git --set-upstream origin && subl .
+git clone git@github.com:georgebonnr/$CLONE_FROM.git $NEW_REPO_NAME && \
+cd ./$NEW_REPO_NAME && git remote rm origin && \
+git remote add origin git@github.com:georgebonnr/$NEW_REPO_NAME.git && \
+git push -u && subl .
+
