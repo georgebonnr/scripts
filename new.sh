60e8deb1f8221d36e4115d28b061312f90caf68f
George Bonner <georgebonnr@gmail.com>
2016-01-25 02:21:21 -0800

    set upstream

--- new.sh
+++ new.sh
@@ -11,4 +11,7 @@ if [ "$MODIFIER" == "npm" ]; then
     CLONE_FROM="npm-es2015-starter"
 fi
 printf "\n Clone source: $CLONE_FROM:\n\n"
-git clone git@github.com:georgebonnr/$CLONE_FROM.git $NEW_REPO_NAME && cd ./$NEW_REPO_NAME && git remote rm origin && git remote add origin git@github.com:georgebonnr/$NEW_REPO_NAME.git && subl .
+git clone git@github.com:georgebonnr/$CLONE_FROM.git $NEW_REPO_NAME\
+ && cd ./$NEW_REPO_NAME && git remote rm origin\
+ && git remote add origin git@github.com:georgebonnr/$NEW_REPO_NAME.git\
+ && git --set-upstream origin && subl .
